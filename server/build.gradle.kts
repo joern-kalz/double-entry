86cf4aad10b5ff0f6ad218c548824668840c922a
Joern Kalz <joern.kalz@gmail.com>
2020-08-15 08:36:39 +0200

    Adds openapi generator to the Spring Boot server

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("org.springframework.boot") version "2.3.2.RELEASE"
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
+    id("org.openapi.generator") version "4.3.1"
     java
 }
 
@@ -14,6 +15,9 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("io.springfox:springfox-swagger2:2.8.0")
+    implementation("org.openapitools:jackson-databind-nullable:0.1.0")
+    implementation("javax.validation:validation-api")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
@@ -21,4 +25,30 @@ dependencies {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+openApiGenerate {
+    generatorName.set("spring")
+    inputSpec.set("$rootDir/openapi.yaml")
+    apiPackage.set("com.github.joern.kalz.doubleentry.generated.api")
+    modelPackage.set("com.github.joern.kalz.doubleentry.generated.model")
+    outputDir.set("$buildDir/generated/openapi")
+    configOptions.set(mapOf(
+        "interfaceOnly" to "true",
+        "skipDefaultInterface" to "true"
+    ))
+}
+
+tasks {
+    named("compileJava").configure {
+        dependsOn(named("openApiGenerate"))
+    }
+}
+
+sourceSets {
+    getByName("main") {
+        java {
+            srcDir("$buildDir/generated/openapi/src/main/java")
+        }
+    }
 }
