25497646f7811c549a74b7f3ce7b8d5a55716967
Joern Kalz <joern.kalz@gmail.com>
2020-08-15 20:21:24 +0200

    Adds jdbc authentication

--- server/src/main/java/com/github/joern/kalz/doubleentry/configuration/WebSecurityConfiguration.java
+++ server/src/main/java/com/github/joern/kalz/doubleentry/configuration/WebSecurityConfiguration.java
@@ -1,15 +1,27 @@
 package com.github.joern.kalz.doubleentry.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .httpBasic().and()
                 .authorizeRequests()
                     .antMatchers("/sign-up").permitAll()
                     .anyRequest().authenticated()
@@ -19,4 +31,15 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     }
 
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
