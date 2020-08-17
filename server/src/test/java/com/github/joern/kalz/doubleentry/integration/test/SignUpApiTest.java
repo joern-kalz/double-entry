c90aac08a4d669b7052b1645134c3a52bdc11b5c
Joern Kalz <joern.kalz@gmail.com>
2020-08-17 06:58:28 +0200

    Switches SignUpApiTest to mockMvc

--- server/src/test/java/com/github/joern/kalz/doubleentry/integration/test/SignUpApiTest.java
+++ server/src/test/java/com/github/joern/kalz/doubleentry/integration/test/SignUpApiTest.java
@@ -1,47 +1,78 @@
 package com.github.joern.kalz.doubleentry.integration.test;
 
-import com.github.joern.kalz.doubleentry.generated.model.ErrorResponse;
-import com.github.joern.kalz.doubleentry.generated.model.SignUpRequest;
-import org.junit.jupiter.api.Assertions;
+import com.github.joern.kalz.doubleentry.model.User;
+import com.github.joern.kalz.doubleentry.model.UsersRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
 public class SignUpApiTest {
 
     @Autowired
-    private TestRestTemplate restTemplate;
+    private WebApplicationContext webApplicationContext;
 
-    @Test
-    public void shouldSignUp() {
-        SignUpRequest request = new SignUpRequest().name("joern").password("secret");
-        ResponseEntity<ErrorResponse> response = restTemplate.postForEntity("/sign-up", request, ErrorResponse.class);
-        Assertions.assertEquals(HttpStatus.CREATED, response.getStatusCode());
+    @Autowired
+    private UsersRepository usersRepository;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .defaultRequest(post("/sign-up")
+                .contentType(MediaType.APPLICATION_JSON))
+                .build();
+
+        usersRepository.deleteAll();
     }
 
     @Test
-    public void shouldFailIfNameMissing() {
-        SignUpRequest request = new SignUpRequest().password("secret");
-        ResponseEntity<ErrorResponse> response = restTemplate.postForEntity("/sign-up", request, ErrorResponse.class);
-        Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    public void shouldSignUp() throws Exception {
+        String requestBody = "{\"name\":\"joern\",\"password\":\"secret\"}";
+
+        mockMvc.perform(post("/sign-up")
+                .content(requestBody))
+                .andExpect(status().isCreated());
+
+        Optional<User> user = usersRepository.findById("joern");
+        assertTrue(user.isPresent());
     }
 
     @Test
-    public void shouldFailIfPasswordMissing() {
-        SignUpRequest request = new SignUpRequest().name("joern");
-        ResponseEntity<ErrorResponse> response = restTemplate.postForEntity("/sign-up", request, ErrorResponse.class);
-        Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    public void shouldFailIfNameMissing() throws Exception {
+        String requestBody = "{\"password\":\"secret\"}";
+
+        mockMvc.perform(post("/sign-up")
+                .content(requestBody))
+                .andExpect(status().isBadRequest());
+
+        Optional<User> user = usersRepository.findById("joern");
+        assertFalse(user.isPresent());
     }
 
     @Test
-    public void shouldFailIfUserAlreadyExists() {
-        SignUpRequest request = new SignUpRequest().name("joern").password("secret");
-        restTemplate.postForEntity("/sign-up", request, ErrorResponse.class);
-        ResponseEntity<ErrorResponse> response = restTemplate.postForEntity("/sign-up", request, ErrorResponse.class);
-        Assertions.assertEquals(HttpStatus.CONFLICT, response.getStatusCode());
+    public void shouldFailIfUserAlreadyExists() throws Exception {
+        String requestBody = "{\"name\":\"joern\",\"password\":\"secret\"}";
+
+        mockMvc.perform(post("/sign-up")
+                .content(requestBody))
+                .andExpect(status().isCreated());
+
+        mockMvc.perform(post("/sign-up")
+                .content(requestBody))
+                .andExpect(status().isConflict());
     }
 }
