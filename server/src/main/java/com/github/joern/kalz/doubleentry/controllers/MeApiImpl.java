e15831862fbeb4e510bf35c184491481f9ca5174
Joern Kalz <joern.kalz@gmail.com>
2020-08-20 05:55:07 +0200

    Creates service layer for users

--- server/src/main/java/com/github/joern/kalz/doubleentry/controllers/MeApiImpl.java
+++ server/src/main/java/com/github/joern/kalz/doubleentry/controllers/MeApiImpl.java
@@ -1,17 +1,13 @@
 package com.github.joern.kalz.doubleentry.controllers;
 
-import com.github.joern.kalz.doubleentry.controllers.exceptions.ParameterException;
 import com.github.joern.kalz.doubleentry.generated.api.MeApi;
 import com.github.joern.kalz.doubleentry.generated.model.GetMeResponse;
 import com.github.joern.kalz.doubleentry.generated.model.UpdateMeRequest;
 import com.github.joern.kalz.doubleentry.model.User;
-import com.github.joern.kalz.doubleentry.model.UsersRepository;
-import com.github.joern.kalz.doubleentry.services.PrincipalProvider;
+import com.github.joern.kalz.doubleentry.services.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -20,32 +16,18 @@ import javax.validation.Valid;
 public class MeApiImpl implements MeApi {
 
     @Autowired
-    private PrincipalProvider principalProvider;
-
-    @Autowired
-    private UsersRepository usersRepository;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    private UserService userService;
 
     @Override
     public ResponseEntity<GetMeResponse> getMe() {
-        String name = principalProvider.getPrincipal().getUsername();
-        return new ResponseEntity<>(new GetMeResponse().name(name), HttpStatus.OK);
+        User user = userService.getMe();
+        GetMeResponse getMeResponse = new GetMeResponse().name(user.getUsername());
+        return new ResponseEntity<>(getMeResponse, HttpStatus.OK);
     }
 
     @Override
-    @Transactional
     public ResponseEntity<Void> updateMe(@Valid UpdateMeRequest updateMeRequest) {
-        User user = principalProvider.getPrincipal();
-
-        if (!passwordEncoder.matches(updateMeRequest.getOldPassword(), user.getPassword())) {
-            throw new ParameterException("old password is incorrect");
-        }
-
-        user.setPassword(passwordEncoder.encode(updateMeRequest.getNewPassword()));
-        usersRepository.save(user);
-
+        userService.updateMyPassword(updateMeRequest.getOldPassword(), updateMeRequest.getNewPassword());
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- server/src/main/java/com/github/joern/kalz/doubleentry/controllers/SignUpApiImpl.java
+++ server/src/main/java/com/github/joern/kalz/doubleentry/controllers/SignUpApiImpl.java
@@ -1,17 +1,12 @@
 package com.github.joern.kalz.doubleentry.controllers;
 
-import com.github.joern.kalz.doubleentry.controllers.exceptions.AlreadyExistsException;
 import com.github.joern.kalz.doubleentry.generated.api.SignUpApi;
 import com.github.joern.kalz.doubleentry.generated.model.SignUpRequest;
-import com.github.joern.kalz.doubleentry.model.AuthoritiesRepository;
-import com.github.joern.kalz.doubleentry.model.Authority;
-import com.github.joern.kalz.doubleentry.model.User;
-import com.github.joern.kalz.doubleentry.model.UsersRepository;
+import com.github.joern.kalz.doubleentry.services.user.CreateUserRequest;
+import com.github.joern.kalz.doubleentry.services.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -19,30 +14,20 @@ import javax.validation.Valid;
 @RestController
 public class SignUpApiImpl implements SignUpApi {
 
-    public static final String DEFAULT_AUTHORITY = "USER";
-
     @Autowired
-    private UsersRepository usersRepository;
-
-    @Autowired
-    private AuthoritiesRepository authoritiesRepository;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    private UserService userService;
 
     @Override
-    @Transactional
     public ResponseEntity<Void> signUp(@Valid SignUpRequest signUpRequest) {
-        String name = signUpRequest.getName();
-        String password = signUpRequest.getPassword();
-
-        if (usersRepository.findById(name).isPresent()) {
-            throw new AlreadyExistsException("username already exists");
-        }
-
-        User createdUser = usersRepository.save(new User(name, password, true));
-        authoritiesRepository.save(new Authority(createdUser, DEFAULT_AUTHORITY));
-
+        CreateUserRequest createUserRequest = createCreateUserRequest(signUpRequest);
+        userService.create(createUserRequest);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    private CreateUserRequest createCreateUserRequest(@Valid SignUpRequest signUpRequest) {
+        CreateUserRequest createUserRequest = new CreateUserRequest();
+        createUserRequest.setName(signUpRequest.getName());
+        createUserRequest.setPassword(signUpRequest.getPassword());
+        return createUserRequest;
     }
 }
