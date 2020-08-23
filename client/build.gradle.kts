7e0e9b4a1eaeab0d981072abc035e07b3317418d
Joern Kalz <joern.kalz@gmail.com>
2020-08-23 07:32:08 +0200

    Adds angular build to client jar

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -1,3 +1,18 @@
+import com.moowork.gradle.node.npm.NpmTask
+
 plugins {
     java
+    id("com.moowork.node") version "1.3.1"
+}
+
+val buildAngular by tasks.registering(NpmTask::class) {
+    setArgs(listOf("--prefix", "src/main/angular", "run", "build"))
+}
+
+tasks {
+    named("jar", Jar::class).configure {
+        dependsOn(buildAngular)
+        from("$buildDir/generated-resources/main")
+        into("static/ui")
+    }
 }
