e0b62c3b4ec834996d13d0a2c8d611c044a9fc01
Joern Kalz <joern.kalz@gmail.com>
2020-08-23 07:40:08 +0200

    Configures openapi generator in client project

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -2,10 +2,18 @@ import com.moowork.gradle.node.npm.NpmTask
 
 plugins {
     java
+    id("org.openapi.generator") version "4.3.1"
     id("com.moowork.node") version "1.3.1"
 }
 
+openApiGenerate {
+    generatorName.set("typescript-angular")
+    inputSpec.set("$rootDir/openapi.yaml")
+    outputDir.set("$projectDir/src/main/angular/src/app/generated/openapi")
+}
+
 val buildAngular by tasks.registering(NpmTask::class) {
+    dependsOn(tasks.named("openApiGenerate"))
     setArgs(listOf("--prefix", "src/main/angular", "run", "build"))
 }
 
