867c666ea4eb1a9562dcae77e904e0e3b051a499
Joern Kalz <joern.kalz@gmail.com>
2020-12-05 19:18:16 +0100

    Improves dashboard design

--- client/src/main/angular/src/app/dashboard/dashboard.component.ts
+++ client/src/main/angular/src/app/dashboard/dashboard.component.ts
@@ -4,11 +4,17 @@ import * as moment from 'moment';
 import { LocalService } from '../local/local.service';
 import { TransactionType } from '../context/context-transaction';
 import { Router } from '@angular/router';
-import { AccountType } from '../account-hierarchy/account-hierarchy';
+import { AccountHierarchy, AccountType } from '../account-hierarchy/account-hierarchy';
 import { saveAs } from 'file-saver';
 import { RepositoryService } from '../generated/openapi/api/repository.service';
-import { Repository } from '../generated/openapi/model/models';
+import { GetAbsoluteBalanceResponse, GetRelativeBalanceResponse, Repository } from '../generated/openapi/model/models';
 import { ApiErrorHandlerService } from '../api-access/api-error-handler.service';
+import { ChartData, ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import { Label, MultiDataSet, SingleDataSet } from 'ng2-charts';
+import { BalancesService } from '../generated/openapi/api/balances.service';
+import { API_DATE } from '../api-access/api-constants';
+import { AccountHierarchyService } from '../account-hierarchy/account-hierarchy.service';
+import { AccountsService } from '../generated/openapi/api/accounts.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -18,15 +24,176 @@ import { ApiErrorHandlerService } from '../api-access/api-error-handler.service'
 export class DashboardComponent implements OnInit {
   AccountType = AccountType;
 
+  accountHierarchy: AccountHierarchy;
+  relativeBalancesList: GetRelativeBalanceResponse[];
+  absoluteBalancesList: GetAbsoluteBalanceResponse[];
+
+  expenseChartInitialized = false;
+  expenseChartLabels: Label[] = [];
+  expenseChartData: ChartDataSets[] = [{data: [], label: '', stack: '1', backgroundColor: '#fff'}];
+
+  revenueChartInitialized = false;
+  revenueChartLabels: Label[] = [];
+  revenueChartData: ChartDataSets[] = [{data: [], label: '', stack: '1', backgroundColor: '#fff'}];
+
+  assetChartInitialized = false;
+  assetChartLabels: Label[] = [];
+  assetChartData: SingleDataSet = [];
+
+  transactionsChartInitialized = false;
+  transactionsChartLabels: Label[] = [];
+  transactionsChartData: ChartDataSets[] = [];
+
+  earningsChartOptions : ChartOptions = {
+    legend: { labels: {fontColor: '#777', boxWidth: 12}, align: 'end'},
+    scales: {
+      xAxes: [{ ticks: {fontColor: '#888'}, gridLines: {display: false} }],
+      yAxes: [{ ticks: {fontColor: '#888', maxTicksLimit: 8}, gridLines: {borderDash: [2]}, stacked: true }],
+    },
+    tooltips: { enabled: false },
+    hover: { mode: null },
+    aspectRatio: 1.6
+  };
+
+  assetChartOptions: ChartOptions = {
+    legend: { position: 'right', align: 'start' },
+    cutoutPercentage: 70,
+    elements: { arc: {borderWidth: 0} },
+    tooltips: { enabled: false },
+    hover: { mode: null },
+    aspectRatio: 1.6
+  };
+
+  transactionsChartOptions : ChartOptions = {
+    legend: { display: false },
+    scales: {
+      xAxes: [{ ticks: {fontColor: '#888'}, gridLines: {display: false} }],
+      yAxes: [{ ticks: {fontColor: '#888', maxTicksLimit: 8}, gridLines: {borderDash: [2]} }],
+    },
+    tooltips: { enabled: false },
+    hover: { mode: null },
+    elements: { line: {tension: 0} },
+    aspectRatio: 1.6,
+  };
+
   constructor(
     private contextService: ContextService,
-    private localService: LocalService,
     private router: Router,
     private repositoryService: RepositoryService,
     private apiErrorHandlerService: ApiErrorHandlerService,
+    private accountsService: AccountsService,
+    private accountHierarchyService: AccountHierarchyService,
+    private balancesService: BalancesService,
+    private localService: LocalService,
   ) { }
 
   ngOnInit(): void {
+    const startEarnings = moment().startOf('month').subtract(12, 'month').format(API_DATE);
+    const startAssets = moment().endOf('year').subtract(20, 'years').format(API_DATE);
+
+    this.accountsService.getAccounts().subscribe(accounts => {
+      this.accountHierarchy = this.accountHierarchyService.createAccountHierarchy(accounts);
+      this.refresh();
+    });
+
+    this.balancesService.getRelativeBalances(startEarnings, 1, 13).subscribe(relativeBalancesList => {
+      this.relativeBalancesList = relativeBalancesList;
+      this.refresh();
+    });
+
+    this.balancesService.getAbsoluteBalances(startAssets, 1, 20 * 12).subscribe(absoluteBalancesList => {
+      this.absoluteBalancesList = absoluteBalancesList;
+      this.refresh();
+    });
+  }
+
+  private refresh() {
+    this.refreshExpense();
+    this.refreshRevenue();
+    this.refreshAsset();
+    this.refreshTransactions();
+  }
+
+  private refreshExpense() {
+    if (!this.accountHierarchy || this.isEmpty(this.relativeBalancesList) || this.expenseChartInitialized) return;
+    this.expenseChartInitialized = true;
+
+    this.expenseChartLabels = this.getEarningLabels();
+    this.expenseChartData = this.getEarningData(AccountType.EXPENSE);
+  }
+
+  private refreshRevenue() {
+    if (!this.accountHierarchy || this.isEmpty(this.relativeBalancesList) || this.revenueChartInitialized) return;
+    this.revenueChartInitialized = true;
+    
+    this.revenueChartLabels = this.getEarningLabels();
+    this.revenueChartData = this.getEarningData(AccountType.REVENUE);
+  }
+
+  private getEarningLabels() {
+    return this.relativeBalancesList.map(balances => this.localService.formatMonth(moment(balances.start)));
+  }
+
+  private getEarningData(accountType: AccountType) {
+    const rootAccount = this.accountHierarchy.root.get(accountType);
+    
+    return this.accountHierarchy.list.get(accountType)
+      .filter(account => account.parentId == rootAccount.id && account.active &&
+        this.relativeBalancesList[0].differences.find(difference => difference.accountId == account.id)
+      )
+      .map(account => ({
+        stack: '1',
+        label: account.name,
+        data: this.relativeBalancesList
+          .map(balances => {
+            const difference = balances.differences.find(difference => difference.accountId == account.id);
+            const amount = difference ? difference.amount : 0;
+            return accountType == AccountType.EXPENSE ? amount : -amount;
+          })
+      }));
+  }
+
+  private refreshAsset() {
+    if (!this.accountHierarchy || this.isEmpty(this.absoluteBalancesList) || this.assetChartInitialized) return;
+    this.assetChartInitialized = true;
+
+    const rootAccount = this.accountHierarchy.root.get(AccountType.ASSET);
+    const childAccounts = this.accountHierarchy.list.get(AccountType.ASSET)
+      .filter(account => account.parentId == rootAccount.id && account.active);
+
+    const lastBalances = this.absoluteBalancesList[this.absoluteBalancesList.length - 1].balances;
+
+    const balances = childAccounts
+      .map(account => lastBalances.find(balance => balance.accountId == account.id))
+      .filter(balance => balance && balance.amount >= 0);
+
+    this.assetChartLabels = balances
+      .map(balance => this.accountHierarchy.accountsById.get(balance.accountId).name);
+
+    this.assetChartData = balances
+      .map(balance => balance.amount);
+  }
+
+  private refreshTransactions() {
+    if (!this.accountHierarchy || this.isEmpty(this.absoluteBalancesList) || this.transactionsChartInitialized) return;
+    this.transactionsChartInitialized = true;
+
+    const rootAccount = this.accountHierarchy.root.get(AccountType.ASSET);
+
+    this.transactionsChartLabels = this.absoluteBalancesList
+      .map(balances => this.localService.formatYear(moment(balances.date)));
+
+    const chartData = this.absoluteBalancesList
+      .map(balancesListEntry => {
+        const balance = balancesListEntry.balances.find(balance => balance.accountId == rootAccount.id);
+        return balance ? balance.amount : 0;
+      });
+
+    this.transactionsChartData = [{ data: chartData, pointRadius: 0 }];
+  }
+
+  private isEmpty(list: any[]) {
+    return !list || list.length == 0;
   }
 
   createGenericTransaction() {
