6c602436bafbf1c63a8ba2ea144a71cf74620a39
Joern Kalz <joern.kalz@gmail.com>
2021-01-25 18:53:20 +0100

    Adds gradle tasks for client tests and analysis

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -29,7 +29,26 @@ val angularNpmBuild by tasks.registering(NpmTask::class) {
     setArgs(listOf("run", "build"))
 }
 
+val angularNpmTest by tasks.registering(NpmTask::class) {
+    setWorkingDir(file("src/main/angular"))
+    setArgs(listOf("test", "sonar", "--no-watch", "--code-coverage"))
+}
+
+val angularNpmSonar by tasks.registering(NpmTask::class) {
+    dependsOn(angularNpmTest)
+    setWorkingDir(file("src/main/angular"))
+    setArgs(listOf(
+        "run", "sonar", "--", 
+        "-Dsonar.host.url=${System.getProperty("sonar.host.url")}",
+        "-Dsonar.login=${System.getProperty("sonar.login")}",
+        "-Dsonar.projectVersion=${project.version}"
+    ))
+}
+
 tasks {
+    named("sonarqube").configure {
+        dependsOn(angularNpmSonar)
+    }
     named("jar", Jar::class).configure {
         dependsOn(angularNpmBuild)
         from("$buildDir/generated-resources/main")
