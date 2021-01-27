8e48182a0f43aca797918f69673c0114372d9331
Joern Kalz <joern.kalz@gmail.com>
2021-01-27 19:58:01 +0100

    Removes unused import

--- client/src/main/angular/src/app/account-name/account-name.component.ts
+++ client/src/main/angular/src/app/account-name/account-name.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { AccountHierarchyNode } from '../account-hierarchy/account-hierarchy-node';
 import { AccountHierarchy, AccountType } from '../account-hierarchy/account-hierarchy';
 
