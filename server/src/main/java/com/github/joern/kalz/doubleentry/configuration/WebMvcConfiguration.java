124725c49a68e77ac188f8b50207175667c39956
Joern Kalz <joern.kalz@gmail.com>
2021-02-08 19:12:35 +0100

    Integrates localized angular ui into server

--- server/src/main/java/com/github/joern/kalz/doubleentry/configuration/WebMvcConfiguration.java
+++ server/src/main/java/com/github/joern/kalz/doubleentry/configuration/WebMvcConfiguration.java
@@ -12,23 +12,30 @@ import java.io.IOException;
 
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
+    private static final String[] UI_LOCALS = new String[] {"en-US", "de-DE"};
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("forward:/ui/index.html");
+        registry.addViewController("/").setViewName("forward:/ui/" + UI_LOCALS[0] + "/index.html");
     }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/ui/**/*")
-                .addResourceLocations("classpath:/static/ui/")
+        for (var local : UI_LOCALS) {
+            addAngularResolver(registry, local);
+        }
+    }
+
+    private void addAngularResolver(ResourceHandlerRegistry registry, String path) {
+        registry.addResourceHandler("/ui/" + path + "/**")
+                .addResourceLocations("classpath:/static/ui/" + path + "/")
                 .resourceChain(true)
                 .addResolver(new PathResourceResolver() {
                     @Override
-                    protected Resource getResource(String resourcePath,
-                                                   Resource location) throws IOException {
+                    protected Resource getResource(String resourcePath, Resource location) throws IOException {
                         Resource requestedResource = location.createRelative(resourcePath);
                         return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
-                                : new ClassPathResource("/static/ui/index.html");
+                                : new ClassPathResource("/static/ui/" + path + "/index.html");
                     }
                 });
     }
